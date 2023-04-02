4c94551345d94731aece626c3ef033a73952277c
Matthew Moir <subcorticalsystems@gmail.com>
2023-04-02 19:15:48 -0400

    Test

---
+++ index.js
@@ -0,0 +1,12 @@
+document.getElementById("explore-btn").onclick = function () {
+    window.location.href = "portfolio.html";
+}
+document.getElementById("portfolio-btn").onclick = function () {
+    window.location.href = "portfolio.html";
+}
+document.getElementById("contact-btn").onclick = function () {
+    window.location.href = "contact.html";
+}
+document.getElementById("about-btn").onclick = function () {
+    window.location.href = "about.html";
+}
